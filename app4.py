abeda8b1dcda26a4de0e332b3e3d4a52dc06f7cd
Manoz Kshetri <152266540+DipsManoz@users.noreply.github.com>
2025-03-25 09:55:19 +0545

    app4.py

--- app4.py
+++ app4.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 import numpy as np
 
 # Load YOLOv8 model
-model = YOLO(r'best (2).pt')
+model = YOLO('best (2).pt')
 
 # Function to detect aircraft in video
 def detect_aircrafts(video_path):
